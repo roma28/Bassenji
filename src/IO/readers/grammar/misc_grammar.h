9c339613c9265345850d1391ccc6e997a4a20b3c
roma28 <ichenkoroma@gmail.com>
2023-07-28 13:42:55 +0200

    Fixed bug with parsing float coordinates in scientific format

--- src/IO/readers/grammar/misc_grammar.h
+++ src/IO/readers/grammar/misc_grammar.h
@@ -22,14 +22,15 @@ struct atom_symbol : pegtl::rep_min_max<1, 2, pegtl::alpha>
 {
 };
 
-struct coord_float : pegtl::seq<pegtl::opt<pegtl::one<'-'>>,
-                                pegtl::plus<pegtl::digit>,
-                                pegtl::one<'.'>,
-                                pegtl::plus<pegtl::digit>>
+struct coord_int : pegtl::seq<pegtl::opt<pegtl::one<'-'>>, pegtl::plus<pegtl::digit>>
 {
 };
 
-struct coord_int : pegtl::seq<pegtl::opt<pegtl::one<'-'>>, pegtl::plus<pegtl::digit>>
+struct coord_float : pegtl::seq<pegtl::opt<pegtl::one<'-'>>,
+                                pegtl::plus<pegtl::digit>,
+                                pegtl::one<'.'>,
+                                pegtl::plus<pegtl::digit>,
+                                pegtl::opt<pegtl::ascii::string<'e'>, coord_int>>
 {
 };
 
