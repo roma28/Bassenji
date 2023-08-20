d67cfe44671731edcec283f7c8d26c4fb4ef860d
roma28 <ichenkoroma@gmail.com>
2023-08-20 14:42:28 +0200

    refactored grammar primitives

--- src/IO/readers/grammar/misc_grammar.h
+++ src/IO/readers/grammar/misc_grammar.h
@@ -7,34 +7,38 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
-//
-//
 
 #ifndef BASSENJI_MISC_GRAMMAR_H
 #define BASSENJI_MISC_GRAMMAR_H
 
 #include <tao/pegtl.hpp>
 
-namespace pegtl = tao::pegtl;
+#define DIGIT_SEQUENCE pegtl::plus<pegtl::digit>
+#define MANTISSA pegtl::sor<pegtl::ascii::string<'e'>, pegtl::ascii::string<'E'>>, pegtl::opt<pegtl::string<'-'>, DIGIT_SEQUENCE>
 
+namespace pegtl = tao::pegtl;
 
 struct atom_symbol : pegtl::rep_min_max<1, 2, pegtl::alpha>
 {
 };
 
-struct coord_int : pegtl::seq<pegtl::opt<pegtl::one<'-'>>, pegtl::plus<pegtl::digit>>
+struct num_int : pegtl::seq<
+    pegtl::opt<pegtl::one<'-'>>,
+    DIGIT_SEQUENCE,
+    pegtl::opt<MANTISSA>>
 {
 };
 
-struct coord_float : pegtl::seq<pegtl::opt<pegtl::one<'-'>>,
-                                pegtl::plus<pegtl::digit>,
-                                pegtl::one<'.'>,
-                                pegtl::plus<pegtl::digit>,
-                                pegtl::opt<pegtl::ascii::string<'e'>, coord_int>>
+struct num_float : pegtl::seq<
+    pegtl::opt<pegtl::one<'-'>>,
+    DIGIT_SEQUENCE,
+    pegtl::one<'.'>,
+    DIGIT_SEQUENCE,
+    pegtl::opt<MANTISSA>>
 {
 };
 
-struct coordinate : pegtl::sor<coord_float, coord_int>
+struct coordinate : pegtl::sor<num_float, num_int>
 {
 };
 #endif //BASSENJI_MISC_GRAMMAR_H
